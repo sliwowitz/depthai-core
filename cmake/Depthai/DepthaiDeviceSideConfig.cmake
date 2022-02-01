b233548cfd69e7db2f96ddcfe699a4d5ae22f38f
alex-luxonis <alex@luxonis.com>
2022-02-01 15:36:13 +0200

    Update BL and FW: support for new OAK-D-Pro-PoE: - BL: no USB3 clock - BL and FW: internal PCIe clock, no clockgen IC

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "33035523070cced071a9ae167df9f0913700294f")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "299fc66773afe4ab24c848c9afb1744a5b3fba25")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
