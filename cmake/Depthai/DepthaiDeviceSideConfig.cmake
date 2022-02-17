deeb8f118a6e72d911e26ed6d40d6d85d1af0c0b
alex-luxonis <alex@luxonis.com>
2022-02-17 02:32:31 +0200

    Update FW: ImageManip: output max width increased 1920 -> 3840

--- cmake/Depthai/DepthaiDeviceSideConfig.cmake
+++ cmake/Depthai/DepthaiDeviceSideConfig.cmake
@@ -2,7 +2,7 @@
 set(DEPTHAI_DEVICE_SIDE_MATURITY "snapshot")
 
 # "full commit hash of device side binary"
-set(DEPTHAI_DEVICE_SIDE_COMMIT "e89359595f669e066ac690799f4fb20f6af16754")
+set(DEPTHAI_DEVICE_SIDE_COMMIT "43de5b283efacbc9fc0da60da61b33169733af59")
 
 # "version if applicable"
 set(DEPTHAI_DEVICE_SIDE_VERSION "")
